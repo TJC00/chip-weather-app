3f8c70fa7f1dc8e5342435e054162d95e369b91d
tjchidanika <tjchidanika@gmail.com>
2023-06-04 16:56:50 +0200

    The Initial Build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains:['wwww.weatherbit.io'],
+    },
+}
 
 module.exports = nextConfig
